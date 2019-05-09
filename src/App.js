f103eff6dfa031ec83d1b7eaab592fe5ab1a0fa4
Ryan O'Keefe <ryan@ryanokeefe.net>
2019-05-09 04:02:44 -0400

    results displaying

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './index.css';
-
 import * as SwiftypeAppSearch from "swiftype-app-search-javascript";
+
 const client = SwiftypeAppSearch.createClient({
   hostIdentifier: process.env.REACT_APP_HOST_IDENTIFIER,
-  apiKey: process.env.REACT_APP_SEARCH_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   engineName: "node-modules"
 });
-// We can query for anything -- <code>foo</code> is our example.
+
 const query = "foo";
 const options = {};
 client.search(query, options)
@@ -17,18 +16,50 @@ client.search(query, options)
   .catch(error => console.log(error))
 
 class App extends Component {
+  state = {
+    response: null
+  };
+  componentWillMount() {
+    this.performQuery("foo");
+  }
+  // query method 
+  performQuery = queryString => {
+    client.search(queryString, {})
+      .then(
+        response => {
+          console.log(response);
+          this.setState({ response });
+        },
+        error => {
+          console.log(`error: ${error}`);
+        }
+      );
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const {response} = this.state;
+    if (!response) return null; 
+  
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Node Module Search
+            </h1>
+          </header>
+          {/* total count of results */}
+          <h2>
+          {response.info.meta.page.total_results} Results
+          </h2>
+          {/* map results for name and disc */}
+          {response.results.map(result => (
+            <div key={result.getRaw("id")}>
+              <p>Name: {result.getRaw("name")}</p>
+              <p>Description: {result.getRaw("description")}</p>
+            </div>
+          ))}
+        </div>
+      );
+
   }
 }
 
